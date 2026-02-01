d71f34a4c3ed545493bb94d39ed691d43d2ad2a3
Mohd Ayan Siddiqui <ayansiddiqui1411@gmail.com>
2026-02-01 19:22:57 +0530

    add prediction

---
+++ src/predict.py
@@ -0,0 +1,20 @@
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.datasets import imdb
+from src.domain_classifier import detect_domain
+from src.model import build_model
+
+word_index=imdb.get_word_index()
+model = build_model()
+
+def predict_sentiment(text):
+    domain = detect_domain(text)
+    tokens = [word_index.get(w,2)for w in text.lower().split()]
+    padded = pad_sequences([tokens],maxlen=150)
+    score = model.predict(padded[0][0])
+
+    return {
+        "domain": domain,
+        "sentiment":"positive" if score>0.5 else"negative",
+        "confidence":round(score,2)
+
+    }
